f04a181fc667bd403a31fb3abc5cb5b4808c6dbc
Emogy-reunion <mv7786986@gmail.com>
2025-01-23 11:47:46 +0300

    create a function that creates and returns the application instance

---
+++ Backend/create_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+'''
+creates the application instance
+initializes it with the configuration settings
+'''
+
+
+def create_app:
+    '''
+    creates the application instance and returns it
+    '''
+    app = Flask(__name__)
+    return app
