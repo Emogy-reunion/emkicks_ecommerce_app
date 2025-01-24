37950e0f1eb84d40403d121a44d760514ebf5bec
Emogy-reunion <mv7786986@gmail.com>
2025-01-24 12:20:20 +0300

    add configuration settings for flask jwt library

--- Backend/config.py
+++ Backend/config.py
@@ -1,5 +1,5 @@
 '''
-stores the app's configuration settings
+istores the app's configuration settings
 '''
 from dotenv import load_dotenv
 import os
@@ -9,3 +9,8 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelata(days=30)
+    JWT_COOKIE_SECURE = True
+    JWT_TOKEN_LOCATION = ['cookies']
