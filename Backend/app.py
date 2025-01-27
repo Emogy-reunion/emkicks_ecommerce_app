d067ce2d3ff28891563d3b388896f4e92aaaeb58
Emogy-reunion <mv7786986@gmail.com>
2025-01-27 11:33:59 +0300

    initialize the jwt library to use it in the application

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,15 @@
-from create_app import create_app
 '''
 initializes the libraries
 creates initial admins
 runs the application
 '''
+from create_app import create_app
+from flask_jwt_extended import JWTManager
 
 
 app = create_app()
+
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
