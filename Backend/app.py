d40f41ae5253f8ac1fd66449e13f72214e772238
Emogy-reunion <mv7786986@gmail.com>
2025-02-28 12:05:23 +0300

    register the auth blueprint so that it can access the app's configuration settings

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,14 @@
-from create_app import create_app
 '''
 initializes the libraries
 creates initial admins
 runs the application
 '''
+from create_app import create_app
+from routes/authentication import auth
 
 
 app = create_app()
+app.register_blueprint(auth)
 
 if __name__ == '__main__':
     app.run(debug=True)
