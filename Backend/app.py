d8e890f29acf79f3db2a06abfd31b1b179e75c20
Emogy-reunion <mv7786986@gmail.com>
2025-03-03 14:07:50 +0300

    fix: fix the main app file

--- Backend/app.py
+++ Backend/app.py
@@ -4,12 +4,11 @@ creates initial admins
 runs the application
 '''
 from create_app import create_app
-from flask_jwt_extended import JWTManager
+from routes.authentication import auth
 
 
 app = create_app()
-
-jwt = JWTManager(app)
+app.register_blueprint(auth)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(debug=True)
