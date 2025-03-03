483b551c5c2f0ed723ed3b263fd1cd4dbcc5fce8
Emogy-reunion <mv7786986@gmail.com>
2025-03-03 14:13:14 +0300

    fix: fix the import error by importing the module from the correct library

--- Backend/routes/authentication.py
+++ Backend/routes/authentication.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from model import Users, db
-from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, jsonify
+from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
 
 
 auth = Blueprint('auth', __main__)
