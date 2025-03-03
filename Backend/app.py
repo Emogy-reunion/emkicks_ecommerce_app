34aa211be56339632d999007f435062513bc81af
Emogy-reunion <mv7786986@gmail.com>
2025-03-03 14:10:58 +0300

    initialize the jwt manager instance with the app instance so that it can access the configuration settings

--- Backend/app.py
+++ Backend/app.py
@@ -5,9 +5,12 @@ runs the application
 '''
 from create_app import create_app
 from routes.authentication import auth
+from flask_jwt_extended import JWTManager
 
 
 app = create_app()
+jwt = JWTManager(app)
+
 app.register_blueprint(auth)
 
 if __name__ == '__main__':
