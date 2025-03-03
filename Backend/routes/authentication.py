57e91f2e0c28e2bf6d89cad5e16b674e2006eec8
Emogy-reunion <mv7786986@gmail.com>
2025-03-03 11:27:13 +0300

    add: add logic to register the user to the database

--- Backend/routes/authentication.py
+++ Backend/routes/authentication.py
@@ -1,5 +1,38 @@
 from flask import Blueprint
-from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
+from model import Users, db
+from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, jsonify
 
 
 auth = Blueprint('auth', __main__)
+
+@auth.route('/register', methods=['POST'])
+def register():
+    '''
+    allows user to create accounts
+    '''
+    data = request.json
+
+    firstname = data['firstname']
+    lastname = data['lastname']
+    email = data['email']
+    username = data['username']
+    phone = data['phone']
+    password = data['password']
+
+
+    user = Users.query.filter_by(email=email).first()
+
+    if user:
+        return jsonify({'error': 'An account associated with this email exists'})
+    else:
+        try:
+            new_user = Users(firstname=firstanme, lastname=lastname,
+                             email=email, username=username, phone=phone,
+                             password=password)
+            db.session.add(new_user)
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'error': 'An unexpected error occured. Please try again'})
+
+        db.session.commit()
+        return jsonify({'success': 'Account created successfully!'})
