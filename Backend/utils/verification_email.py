f047ad54583744651b739fd40f852c9e059e8ab3
Emogy-reunion <mv7786986@gmail.com>
2025-03-05 19:13:28 +0300

    add: create a function that sends the verification email to the user

---
+++ Backend/utils/verification_email.py
@@ -0,0 +1,19 @@
+from flask import url_for, render_template
+from flask_mail import Message
+from app import mail
+
+def send_verification_email(user):
+    '''
+    sends an email with the verification token to the user
+    '''
+    verification_token = user.generate_verification_token()
+    verification_url = url_for('https://mark.com/verify', verification_token=verification_token, _external=True)
+
+    msg = Message(
+            subject='VERIFY YOUR EMAIL!',
+            recipients=[user.email],
+            sender='info.bytevision@gmail.com'
+            )
+    msg.body = f"Click the following link to verify your email: {verification_url}. The link expires in 60 minutes"
+    msg.html = render_template('verification.html', username=user.username)
+    mail.send(msg)
