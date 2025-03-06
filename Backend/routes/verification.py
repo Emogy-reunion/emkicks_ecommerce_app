7afefef3203587066e47c416be75649f6f4df5bf
Emogy-reunion <mv7786986@gmail.com>
2025-03-06 09:46:38 +0300

    add: add route that checks if the user token is valid and updates the user verification status

--- Backend/routes/verification.py
+++ Backend/routes/verification.py
@@ -1,7 +1,25 @@
 '''
 Handles token verification via email
 '''
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from model import db, Users
 
 verify = Blueprint('verify', __name__)
 
+@verify.route('/verify_email/<token>', methods=['POST'])
+def verify_email(token):
+    '''
+    checks if the token is valid
+    updates the user verification status to true if the token is valid
+    '''
+    user = Users.verify_email_verification_token(token)
+
+    if user:
+        try:
+            user.verified = True
+        except Exception as e:
+            return jsonify({'error': 'An unexpected error occured. Please try again later!'})
+        db.session.commit()
+        return jsonify({'success': 'Verified successfully!'})
+    else:
+        return jsonify({'error': ' User verification failed. Please try again!'})
