92ec018746959e5de40ddb03e674c084e7ba8114
Emogy-reunion <mv7786986@gmail.com>
2025-03-06 12:28:11 +0300

    add: register the reset blueprint so that it can access the app's context

--- Backend/app.py
+++ Backend/app.py
@@ -8,6 +8,7 @@ from routes.authentication import auth
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from routes.verification import verify
+from routes.reset_password import reset
 
 
 app = create_app()
@@ -16,6 +17,7 @@ mail = Mail(app)
 
 app.register_blueprint(auth)
 app.register_blueprint(verify)
+app.register_blueprint(reset)
 
 if __name__ == '__main__':
     app.run(debug=True)
