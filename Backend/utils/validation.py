5e3013773f1e2cac420cb61e53317082c1ddb632
Emogy-reunion <mv7786986@gmail.com>
2025-03-07 18:19:46 +0300

    add: create function that checks if the email format and domain are valid

--- Backend/utils/validation.py
+++ Backend/utils/validation.py
@@ -1,3 +1,4 @@
+from email_validator import validate_email, EmailNotFoundError
 import re
 
 def validate_firstname(firstname):
@@ -9,7 +10,7 @@ def validate_firstname(firstname):
         errors.append('First name is required!')
     elif len(firstname) < 2:
         errors.append('First name must be at least 2 characters long!')
-    elif len(firstname) > 50:
+    elif len(firstname) > 30:
         errors.append('First name must not exceed  30 characters long!')
     return errors
 
@@ -23,6 +24,25 @@ def validate_lastname(lastname):
         errors.append('Last name is required!')
     elif len(lastname) < 2:
         errors.append('Last name must be at least 2 characters long!')
-    elif len(lastname) > 50:
-        errors.append('Last name must not exceed 30 characters long!'0
+    elif len(lastname) > 30:
+        errors.append('Last name must not exceed 30 characters long!')
+    return errors
+
+def check_email(email):
+    '''
+    makes the sure the email format is correct
+    ensures that the  domain exists
+    '''
+    errors = []
+    if not email:
+        errors.append('Email is required!')
+    elif len(email) > 5:
+        errors.append('Email must be at least 5 characters long!')
+    elif len(email) > 30:
+        errors.append('Email must not exceed 30 characters long!')
+    else:
+        try:
+            valid = validate_email(email, check_delivaribility=True)
+        except EmailNotFoundError as e:
+            errors.append('Invalid email format!')
     return errors
