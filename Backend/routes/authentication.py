4d6ad79953e079ad977843c871f39ec6b397b2b7
Emogy-reunion <mv7786986@gmail.com>
2025-03-07 10:50:19 +0300

    add: add validation checks for the data entered by the user

--- Backend/routes/authentication.py
+++ Backend/routes/authentication.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from model import Users, db
+from email_validator import validate_email, EmailNotFoundError
 from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
 from utils.verification_email import send_verification_email
 
@@ -11,6 +12,7 @@ def register():
     '''
     allows user to create accounts
     '''
+    RESERVED_USERNAMES = ['root', 'admin', 'moderator', 'support', 'null', 'undefined']
     data = request.json
 
     firstname = data['firstname']
@@ -20,11 +22,54 @@ def register():
     phone = data['phone']
     password = data['password']
 
+    errors = {}
+    
+    if not firstname:
+        errors['firstname'] = 'First name is required!'
+    elif len(firstname) < 2:
+        errors['firstname'] = 'First name is too short. First name should be 2 or more characters long!'
+    elif len(firstname) > 50:
+        errors['firstname'] = 'First name is too long. First name should be less than 50 characters long!'
+
+    if not lastname:
+        errors['lastname'] = 'Last name is required!'
+    elif len(lastname) < 2:
+        errors['lastname'] = 'Last name is too short. Last name should be 2 or more characters long!'
+    elif len(lastname) > 50:
+        errors['lastname'] = 'Last name is too long. Lastname name should be less than 50 characters long!'
+
+    if not email:
+        errors['email'] = 'Email is required!'
+    elif len(email) < 5:
+        errors['email'] = 'Email is too short. It should be more than 5 characters!'
+    elif len(email) > 50:
+        errors['email'] = 'Email is too long. It should not exceed 50 characters!'
+    else:
+        try:
+            valid = validate_email(email, check_delivaribility=True)
+        except EmailNotValidError as e:
+            errors['email'] = 'Invalid email format!'
+
+    if not username:
+        errors['username'] = 'Username is required!'
+    elif len(username) < 5:
+        errors['username'] = 'Username is too short. Username should be 5 or more characters long!'
+    elif len(username) > 50:
+        errors['username'] = 'Username is too long. Username should not exceed 50 characters'
+    elif username.lower() in RESERVED_USERNAMES:
+        errors['username'] = 'Username is associated with another account, please pick another one!'
+
+
+
+
 
     user = Users.query.filter_by(email=email).first()
+    member = Users.query.filter_by(username=username).first()
 
     if user:
         return jsonify({'error': 'An account associated with this email exists!'})
+    elif member:
+        return jsonify({'error': 'An account associated with this username exists!'})
     else:
         try:
             new_user = Users(firstname=firstname, lastname=lastname,
