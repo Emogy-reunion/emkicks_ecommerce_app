dd290d94e447cca6a67b4910810a8f4c46c88303
Emogy-reunion <mv7786986@gmail.com>
2025-03-23 22:47:35 +0300

    fix: use try except to catch errors that happen during data fetching to prevent app from breaking

--- Backend/routes/user_collections.py
+++ Backend/routes/user_collections.py
@@ -77,7 +77,7 @@ def women_sneakers_preview():
                 .all()
         paginated_results = sneakers.paginate_results(page=page, per_page=per_page)
     except Exception as e:
-        return jsonify({"error": 'An unexpected error occured. Please try again!'})
+        return jsonify({"error": 'An unexpected error occured. Please try again!'}), 500
 
     if not paginated_results.items:
         return jsonify({'error': 'No sneakers available at the moment. Stay tuned for new arrivals!'}), 404
@@ -117,12 +117,18 @@ def kids_sneakers_preview():
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 15, type=int)
 
-    sneakers = Sneakers.query \
-            .filter(Sneakers.category == 'kids') \
-            .order_by(Sneakers.id.desc()) \
-            .options(selectinload(Sneakers.images)) \
-            .all()
-    paginated_results = sneakers.paginate(page=page, per_page=per_page)
+    paginated_results = None
+
+    try:
+        sneakers = Sneakers.query \
+                .filter(Sneakers.category == 'kids') \
+                .order_by(Sneakers.id.desc()) \
+                .options(selectinload(Sneakers.images)) \
+                .all()
+        paginated_results = sneakers.paginate(page=page, per_page=per_page)
+    except Exception as e:
+        return jsonify({"error": 'An unexpected error occured. Please try again!'}), 500
+    
 
     if not paginated_results.items:
         return jsonify({'error': 'No sneakers available at the moment. Stay tuned for new arrivals!'}), 404
