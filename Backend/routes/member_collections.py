7ab6ce2fad8637ffb1d247989a882d2622c3058d
Emogy-reunion <mv7786986@gmail.com>
2025-03-27 13:59:01 +0300

    add: add a route that retrieves kids sneakers preview from the database for logged in users

--- Backend/routes/member_collections.py
+++ Backend/routes/member_collections.py
@@ -60,9 +60,9 @@ def member_men_sneakers_preview():
 
 @jwt_required()
 @post.route('/member_women_sneaker_preview', method=['GET'])
-def member_women_sneaker_preview():
+def member_women_sneakers_preview():
     '''
-    retrieves sneaker previews for women sneakers
+    retrieves women sneaker previews for logged in users
     '''
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 15, type=int)
@@ -102,6 +102,55 @@ def member_women_sneaker_preview():
                     'total': paginated_results.total,
                     'next': paginated_results.next_num if paginated_results.has_next else None,
                     'previous': paginated_results.prev_num if paginted_results.has_prev else None
+                    }
+                }
+        return jsonify(response), 200
+
+@jwt_required()
+@post.route('/member_kids_sneakers_preview', methods=['GET'])
+def member_kids_sneakers_preview():
+    '''
+    retrieves the kids sneakers preview for logged in users
+    '''
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 15, type=int)
+
+    paginated_results = None
+
+    try:
+        sneakers = Sneaker.query\
+                .filter(Sneakers.category == 'kids')\
+                .order_by(Sneakers.id.desc())\
+                .options(selectinload(Sneakers.images))\
+                .all()
+        paginated_results = sneakers.paginate(page=page, per_page=per_page)
+    except Exception as e:
+        return jsonify({"error": 'An unexpected error occured. Please try again!'}), 500
+
+    if not paginated_results.items:
+        return jsonify("error": 'No sneaker available at the momen. Stay tuned for new arrivals!'}), 404
+    else:
+        sneakers = [
+                {
+                    'name': sneaker.name,
+                    'sneaker_id': sneaker.id,
+                    'price': sneaker.final_price,
+                    'discount': sneaker.discount_rate,
+                    'original_price': sneaker.original_price,
+                    'status': sneaker.status,
+                    'image': sneaker.images[0].filename if sneaker.images else None
+                    }
+                for sneaker in paginated_results.items
+                ]
+        response = {
+                'sneakers': sneakers,
+                'pagination': {
+                    'page': paginated_results.page,
+                    'per_page': paginated_results.per_page,
+                    'pages': paginated_results.pages,
+                    'total': paginated_results.total,
+                    'next': paginated_results.next_num if paginated_results.has_next else None,
+                    'previous': paginated_results.prev_num if paginated_results.has_prev else None
                     }
                 }
         return jsonify(response), 200
