ac441fb3b999184fd74b79a487a4b917661f81e8
Emogy-reunion <mv7786986@gmail.com>
2025-06-26 12:25:35 +0300

    - Initialize extensions (Bcrypt, SQLAlchemy, Mail) by binding them to the Flask app inside create_app(),  Ensures extensions work properly with the app factory pattern

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 '''
 creates the application instance
 initializes it with the configuration settings
 '''
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+mail = Mail()
 
 
 def create_app():
@@ -12,4 +19,8 @@ def create_app():
     '''
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    bcrypt.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
     return app
