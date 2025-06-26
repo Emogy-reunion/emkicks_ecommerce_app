e2fc0c4861bf0d1f552fdd5b6a6631c53e403592
Emogy-reunion <mv7786986@gmail.com>
2025-06-26 12:41:55 +0300

    refactor: initialize the jwt when the app is created

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_jwt_extended import JWTManager
 '''
 creates the application instance
 initializes it with the configuration settings
@@ -11,6 +12,7 @@ initializes it with the configuration settings
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 mail = Mail()
+jwt = JWTManager()
 
 
 def create_app():
@@ -20,7 +22,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    bcrypt.init_app(app)
     db.init_app(app)
+    bcrypt.init_app(app)
     mail.init_app(app)
+    jwt.init_app(app)
     return app
